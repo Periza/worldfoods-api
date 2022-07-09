5a79140d1cd6ad3bceacac91f71b9e06ab505112
Marko Perica <marko.perica2@gmail.com>
2022-07-09 14:13:52 +0200

    Added seeder helper traits

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\Traits\TruncateTable;
+use Database\Seeders\Traits\DisableForeginKeys;
 
 class CategorySeeder extends Seeder
 {
+    use TruncateTable, DisableForeginKeys;
     /**
      * Run the database seeds.
      *
@@ -14,6 +17,10 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Disable foreign key checks
+        $this->disableForeignKeys();
+        // Truncate table
+        $this->truncate('categories');
+        $this->enableForeignKeys();
     }
 }
