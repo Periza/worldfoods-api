3e60b17d43de275358942529988b672a0329d0ce
Marko Perica <marko.perica2@gmail.com>
2022-07-13 19:50:52 +0200

    return response based on with parameters

--- app/Http/Resources/MealResource.php
+++ app/Http/Resources/MealResource.php
@@ -12,20 +12,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class MealResource extends JsonResource
 {
-
-    /* public function __construct($resource) {
-        $this->pagination = [
-            'total' => $resource->total(),
-            'count' => $resource->count(),
-            'per_page' => $resource->perPage(),
-            'current_page' => $resource->currentPage(),
-            'total_pages' => $resource->lastPage()
-        ];
-
-        $resource = $resource->getCollection();
-
-        parent::__construct($resource);
-    } */
     /**
      * Transform the resource into an array.
      *
@@ -42,25 +28,46 @@ class MealResource extends JsonResource
             return strtotime($time) > $request->diff_time;
         };
 
-        if($greaterThanDiffTime($this->created_at) || $greaterThanDiffTime($this->updated_at) || $greaterThanDiffTime($this->deleted_at)) {
-            if($this->deleted_at > $this->updated_at && $this->deleted_at > $this->created_at) {
-                $status = "deleted";
-            } else if($this->updated_at > $this->deleted_at && $this->updated_at > $this->created_at) {
-                $status = "updated";
+        if($request->has('diff_time')) {
+            if($greaterThanDiffTime($this->created_at) || $greaterThanDiffTime($this->updated_at) || $greaterThanDiffTime($this->deleted_at)) {
+                if($this->deleted_at > $this->updated_at && $this->deleted_at > $this->created_at) {
+                    $status = "deleted";
+                } else if($this->updated_at > $this->deleted_at && $this->updated_at > $this->created_at) {
+                    $status = "updated";
+                }
             }
         }
 
         // set locale
         app()->setlocale($request->lang);
 
-        
+        $ingredients = false;
+        $category = false;
+        $tags = false;
+
+        // check if with is present and not empty
+        if($request->has('with')) {
+            $with_array = explode(',', $request->input('with'));
+            if(in_array('category', $with_array)) {
+                $category = true;
+            }
+            if(in_array('ingredients', $with_array)) {
+                $ingredients = true;
+            }
+            if(in_array('tags', $with_array)) {
+                $tags = true;
+            }
+
+        }
+
+
 
         return [
             'id' => $this->id,
             'status' => $status,
-            'category' => $this->category()->getResults(),
-            'tags' => $this->tags()->getResults(),
-            'ingredients' => $this->ingredients()->getResults()
+            'category' => $this->when($category, $this->category()->getResults()),
+            'tags' => $this->when($tags, $this->tags()->getResults()),
+            'ingredients' => $this->when($ingredients, $this->ingredients()->getResults())
         ];
     }
 }
